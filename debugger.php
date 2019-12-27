30395d810797766ef2a4d3570c3b47d5a419a225
Grisha Ivanetc <59262523+deCodedLife@users.noreply.github.com>
2019-12-28 01:08:19 +0300

    Rewrite
    Create class system

--- debugger.php
+++ debugger.php
@@ -1,30 +1,24 @@
 <?php
 
-require ("security.php"); # import security php file
+class Debugger {
 
-function debug( sting $from, string $message, int $type = 0 ) {  # main funcion
+  public function debug( string $from, string $message, int $type = 0 ) {  # main funcion
 
-  $from     = getParsed( $from );     # get normal string from error string ( I'M_A_"echo "Hacker";-- | to IM_A_echo Hacker -- )
-  $message  = getParsed( $message );  # get normal string from error string ( from messages )
+    switch ( $type ) {  # get type
+      case 0: # plain message type
+        echo "{Message}[".$from."] " . $message;  # display data
+        break;
+      case 1: # warning
+        echo "{Warning}[".$from."] " . $message;  # display data
+        break;
+      case 2: # error
+        echo "{Critical}[".$from."] " . $message; # display data
+        break;
+      default:  # plain text
+        echo "{Message}[".$from."] " . $message;  # display data
+        break;
+    }
 
-  if ( !parse( $from ) || !parse( $message ) ) {  # checking message for denied sumbols
-    echo "[Debbuger] error: exeption was occured!"; # just display message
-    return; # stop script
-  }
-
-  switch ( $type ) {  # get type
-    case 0: # plain message type
-      echo "{Message}[".$from."] " . $message ;  # display data
-      break;
-    case 1: # warning
-      echo "{Warning}[".$from."] " . $message ;  # display data
-      break;
-    case 2: # error
-      echo "{Critical}[".$from."] " . $message ; # display data
-      break;
-    default:  # plain text
-      echo "{Message}[".$from."] " . $message ;  # display data
-      break;
   }
 
 }
