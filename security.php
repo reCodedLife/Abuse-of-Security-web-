64fe13bbd21d925ca290f1093635b4e8ef198467
Grisha Ivanetc <59262523+deCodedLife@users.noreply.github.com>
2019-12-28 01:08:46 +0300

    Rewrite
    Create class system

--- security.php
+++ security.php
@@ -1,31 +1,74 @@
 <?php
 
-$symbols = array( '"', "'", "\\" ); # denied symbols
+date_default_timezone_set( "Europe/Moscow" );
+header('Content-Type: text/html; charset=utf-8');
 
-function getParsed ( string $string ) {    # get normal string from error string
+class Security {
 
-  $normal = "";                     # empty string
-  $parseString  = preg_split('//u', $string,   NULL, PREG_SPLIT_NO_EMPTY);  # convert to array
+  public $symbols = array( '"', "'", '\\' ); # denied symbols
+  public $debugger;
+  public $library = "Security";
 
-  for ($i = 0; $i < count( $parseString ); $i++) {  # create a cycle with count of string
-    if ( !in_array( $parseString[$i], $symbols ) )  # checking string for denied symbols
-      $normal = $normal . $parseString[$i];         # add normal symbols to string
+  function __construct() {
+    $this->debugger = new Debugger();
   }
 
-  return $normal; # return normal string
-}
+  public function getParsed ( string $string ) {     # get normal string from error string
+    if ( $string != "" ) {
 
-function parse( string $string ) { # checking for denied symbols
+      $normal = "";                     # empty string
+      $parseString  = preg_split('//u', $string,   NULL, PREG_SPLIT_NO_EMPTY);  # convert to array
+      for ($i = 0; $i < count( $parseString ); $i++) {  # create a cycle with count of string
+        if ( !in_array( $parseString[$i], $symbols ) )  # checking string for denied symbols
+          $normal = $normal . $parseString[$i];         # add normal symbols to string
+      }
 
-  $allow = true;            # checking variable
-  $parseString  = preg_split('//u', $string,   NULL, PREG_SPLIT_NO_EMPTY);  # convert to array
+      return $normal; # return normal string
 
-  for ($i = 0; $i < count($parseString); $i++) {    # create a cycle with count of string
-    if ( in_array( $parseString[$i], $symbols ) )   # checking string for denied symbols
-      $allow = false;                 # change variable to false
+    } else {
+      $this->debugger->debug( $this->library, "String not given", 2 );
+      return;
+    }
+
   }
 
-  return allow; # return allow string
+  public function parse( string $string ) { # checking for denied symbols
+
+    $allow = true;            # checking variable
+    $parseString  = preg_split('//u', $string,   NULL, PREG_SPLIT_NO_EMPTY);  # convert to array
+
+    for ($i = 0; $i < count($parseString); $i++) {    # create a cycle with count of string
+      if ( in_array( $parseString[$i], $this->symbols ) )   # checking string for denied symbols
+        $allow = false;                 # change variable to false
+    }
+
+    return $allow; # return allow string
+  }
+
+  public function getMetadata( string $token ) { # get metadata from token
+    /*
+      TOKEN STRUCTURE:
+
+      time*name*password
+
+      time contains date of token
+      name contains login of user
+      password contains hashed user password
+    */
+    $array = explode( "*", base64_decode($token) ); // decode token and split to array *WARNING password is heshed!
+
+    if ( !$this->parse( $array[0] ) || !$this->parce( $array[1] ) || !$this->parse( $array[2] ) ) {  # checking token for denied symbols
+      $this->debugger->debug( $this->library, "Token contains denied symbols", 2 );  #debug error
+      return; # stop script
+    }
+
+    $metaData->time = $array[0];  # write timestamp to metadata
+    $metaData->name = $array[1];  # write username to metadata
+    $metaData->pass = $array[2];  # write password to metadata
+
+    return $metaData; # just return metadata
+
+  }
 }
 
 ?>
