4d58200182350ab04b0e14676b57b866b8375bd0
Grisha Ivanetc <59262523+deCodedLife@users.noreply.github.com>
2019-12-31 23:13:36 +0300

    Update debugger.php
    Updates...

--- debugger.php
+++ debugger.php
@@ -4,6 +4,8 @@ class Debugger {
 
   public function debug( string $from, string $message, int $type = 0 ) {  # main funcion
 
+    $message = $message . "<br/>";
+
     switch ( $type ) {  # get type
       case 0: # plain message type
         echo "{Message}[".$from."] " . $message;  # display data
@@ -12,7 +14,8 @@ class Debugger {
         echo "{Warning}[".$from."] " . $message;  # display data
         break;
       case 2: # error
-        echo "{Critical}[".$from."] " . $message; # display data
+        die(  "{Critical}[".$from."] " . $message ); # display data
+        exit(0);
         break;
       default:  # plain text
         echo "{Message}[".$from."] " . $message;  # display data
